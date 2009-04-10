03713cc9e06cace3b545e426ec9fce876a0ec48d
jakob <jakob@a8bed3e6-0eea-4999-a5e1-f6419deda748>
2009-04-10 18:36:00 +0000

    - Added initial directory structure - Added basic Makefile - Added basic main source file
    git-svn-id: svn://localhost/mossofs@1 a8bed3e6-0eea-4999-a5e1-f6419deda748

---
+++ trunk/src/mossofs.c
@@ -0,0 +1,31 @@
+/*
+ * This file is part of Mossofs.
+ *
+ * Mossofs is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License as
+ * published by the Free Software Foundation; version 3 of the
+ * License.
+ *
+ * Mossofs is distributed in the hope that it will be useful, but
+ * WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+ * General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with Mossofs; if not, write to the Free Software
+ * Foundation, Inc., 51 Franklin St, Fifth Floor, Boston, MA
+ * 02110-1301  USA
+ *   
+ * Copyright (C) 2009 Jakob Westhoff <jakob@westhoffswelt.de>
+ */
+
+#include <fuse.h>
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <fcntl.h>
+
+int main( int argc, char **argv )
+{
+    return 0;
+}
