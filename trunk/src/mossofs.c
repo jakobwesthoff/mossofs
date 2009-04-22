5d2f1a978a86c1b4ca70d2407f38ab55c7cfed63
jakob <jakob@a8bed3e6-0eea-4999-a5e1-f6419deda748>
2009-04-22 17:54:36 +0000

    - Implemented first test version of fuse directory listing functions # This does not work 100% correctly at the moment. Furthermore the retrieved # data is not cached at all.
    git-svn-id: svn://localhost/mossofs@36 a8bed3e6-0eea-4999-a5e1-f6419deda748

--- trunk/src/mossofs.c
+++ trunk/src/mossofs.c
@@ -30,6 +30,160 @@
 
 #include "mosso.h"
 
+static char* mosso_username = NULL;
+static char* mosso_key      = NULL;
+
+#define MOSSO_CONNECTION(m) \
+    mosso_connection_t* m = NULL; \
+    { struct fuse_context* context = fuse_get_context(); m = (mosso_connection_t*)context->private_data;};
+
+#define INIT_DEBUGLOG debuglog = fopen( "debug.log", "a" )
+#define DEBUGLOG(s, ...) fprintf( debuglog, s, ##__VA_ARGS__ ); fflush( debuglog )
+FILE* debuglog = NULL;
+
+/**
+ * Initialize the mosso filesystem
+ *
+ * This function establishes a connection to the mosso cloud service and
+ * retrieves the needed auth token. 
+ *
+ * The mosso connection struct will be returned to allow fuse to pass it to any
+ * other called function.
+ */
+static void* mossofs_init( struct fuse_conn_info *conn ) 
+{
+    mosso_connection_t* mosso = NULL;
+
+    // Initialize the cURL library enabling SSL support
+    curl_global_init( CURL_GLOBAL_SSL );
+
+    if ( ( mosso = mosso_init( mosso_username, mosso_key ) ) == NULL )
+    {
+        printf( "The connection to Mosso Cloudspace could not be established: %s\n", mosso_error_string() );
+        curl_global_cleanup();
+        exit( 2 );
+    }
+
+    // Return the connection to embed it into every fuse context.
+    return mosso;
+}
+
+/**
+ * Called upon filesystem destruction
+ *
+ * The supplied private data is the mosso connection structure created during
+ * init.
+ */
+static void mossofs_destroy( void* mosso ) 
+{
+    mosso_cleanup( ( mosso_connection_t* )mosso );
+    curl_global_cleanup();
+}
+
+/**
+ * Retrieve attributes of the given path
+ */
+static int mossofs_getattr( const char *path, struct stat *stbuf ) 
+{
+    MOSSO_CONNECTION( mosso );
+    mosso_object_meta_t* meta = NULL;    
+
+    int id = rand();
+
+    DEBUGLOG( "getattr(%d): %s\n",id, path );
+
+    // Null the stats buffer
+//    memset( stbuf, 0, sizeof( struct stat ) );
+
+    if ( strcmp( path, "/" ) == 0 ) 
+    {
+        stbuf->st_mode  = S_IFDIR | 0755;
+        stbuf->st_nlink = 2; /* Link into the dir and link inside the dir (.) */
+        return 0;
+    }
+    
+    // Try to retrieve meta information for the given filepath
+    if ( ( meta = mosso_get_object_meta( mosso, (char*)path ) ) == NULL ) 
+    {
+        // The requested object is not existant
+        return -ENOENT;
+    }
+
+    // Set the correct file/dir type
+    if ( meta->type == MOSSO_OBJECT_TYPE_OBJECT ) 
+    {
+        stbuf->st_mode  = S_IFREG | 0444;
+        stbuf->st_nlink = 2; /* Link into the dir and link inside the dir (.) */
+        stbuf->st_size  = meta->size;
+    }
+    else 
+    {
+        stbuf->st_mode  = S_IFDIR | 0755;
+        stbuf->st_nlink = 1;
+
+        // If it is a container a size is available
+        if ( meta->type == MOSSO_OBJECT_TYPE_CONTAINER ) 
+        {
+            stbuf->st_size = meta->size;
+        }
+    }   
+
+    mosso_object_meta_free( meta );
+
+    /* Everything okey */
+    return 0;
+}
+
+/** 
+ * Called whenever a directory contents needs to be listed
+ */
+static int mossofs_readdir( const char *path, void *buf, fuse_fill_dir_t filler, off_t offset, struct fuse_file_info *fi ) 
+{
+    MOSSO_CONNECTION( mosso );
+    mosso_object_t* objects = NULL;
+
+    int id = rand();
+    int count = 0;
+
+    DEBUGLOG( "readdir(%d): %s\n", id, path );
+
+    if ( ( objects = mosso_list_objects( mosso, (char*)path, &count ) ) == NULL ) 
+    {
+        DEBUGLOG( "  path does not exist\n" );
+        return -ENOENT;
+    }
+
+    DEBUGLOG( "  %d items retrieved\n", count );
+
+    DEBUGLOG( "  filling(%d): %s\n", id, "." );
+    filler( buf, ".", NULL, 0 );
+    DEBUGLOG( "  filling(%d): %s\n", id, ".." );
+    filler( buf, "..", NULL, 0 );
+
+    {
+        mosso_object_t* cur = objects->root;
+        while( cur != NULL ) 
+        {
+            DEBUGLOG( "  filling(%d): %s\n", id, cur->name );
+            filler( buf, cur->name, NULL, 0 );
+            if ( cur->next != NULL ) 
+            {
+                DEBUGLOG( "Next object entry is available\n" );                
+            }
+            else 
+            {
+                DEBUGLOG( "Next object is NULL\n" );
+            }
+            cur = cur->next;
+        }
+    }
+
+    mosso_object_free_all( objects );
+
+    /* Everything okey */
+    return 0;
+}
+
 /**
  * Show the usage message of this application
  */
@@ -50,10 +204,7 @@ static void show_usage( char* executable )
  */
 int main( int argc, char **argv )
 {
-    char* username = NULL;
-    char* key = NULL;
-
-    mosso_connection_t* mosso = NULL;
+    INIT_DEBUGLOG;
 
     // Define all possible commandline options
     struct option long_options[] =
@@ -71,111 +222,33 @@ int main( int argc, char **argv )
             switch ( option )
             {
                 case 'u':
-                    username = strdup( optarg );
+                    mosso_username = strdup( optarg );
                 break;
                 case 'k':
-                    key = strdup( optarg );
+                    mosso_key = strdup( optarg );
                 break;
             }
         }
 
-        if ( username == NULL || key == NULL || argc != optind + 1 )
+        if ( mosso_username == NULL || mosso_key == NULL || argc != optind + 1 )
         {
             show_usage( argv[0] );
             exit( 1 );
         }
     }
 
-    // Initialize the cURL library enabling SSL support
-    curl_global_init( CURL_GLOBAL_SSL );
-
-    if ( ( mosso = mosso_init( username, key ) ) == NULL )
+    // Initialize and call the fuse handler
     {
-        printf( "The connection to Mosso Cloudspace could not be established: %s\n", mosso_error_string() );
-        curl_global_cleanup();
-        exit( 2 );
+        struct fuse_operations mossofs_operations = 
+        {
+            .init    = mossofs_init,
+            .destroy = mossofs_destroy,
+            .getattr = mossofs_getattr,
+            .readdir = mossofs_readdir,
+        };
+
+        // optind (-/+) 1 is an ugly hack. A syntax to specify the mossofs
+        // filesystem with one initial parameter needs to be introduced here.
+        return fuse_main( argc - optind + 1, argv + optind - 1, &mossofs_operations, NULL );
     }
-/*
-    {
-        mosso_object_t* object = NULL;
-        mosso_object_t* cur    = NULL;
-        int count = 0;
-
-        if ( ( object = mosso_list_objects( mosso, *(argv + optind), &count ) ) == NULL )
-        {
-            if ( mosso_error() != MOSSO_ERROR_NOCONTENT ) 
-            {
-                printf( "Container listing failed: %s\n", mosso_error_string() );
-                curl_global_cleanup();
-                exit( 2 );
-            }
-            else 
-            {
-                printf( "%s\n", mosso_error_string() );
-            }
-        }
-
-        printf( "%d objects fetched\n", count );
-
-        if ( count != 0 ) 
-        {
-            cur = object->root;
-            while( cur != NULL )
-            {
-                printf( "Name: %s, RequestPath: %s\n", cur->name, cur->request_path );
-                cur = cur->next;
-            }
-            mosso_object_free_all( object );
-        }
-    }
-*/
-    {
-        mosso_object_meta_t* meta = mosso_get_object_meta( mosso, *(argv + optind) );
-        if ( meta != NULL ) 
-        {
-            printf( "name: %s\n", meta->name );
-            printf( "request_path: %s\n", meta->request_path );
-            switch( meta->type ) 
-            {
-                case MOSSO_OBJECT_TYPE_CONTAINER:
-                    printf( "type: CONTAINER\n" );
-                break;
-                case MOSSO_OBJECT_TYPE_OBJECT:
-                    printf( "type: OBJECT\n" );
-                break;
-                case MOSSO_OBJECT_TYPE_VDIR:
-                    printf( "type: VDIR\n" );
-                break;
-            }
-            printf( "content_type: %s\n", meta->content_type );
-            unsigned char* checksum = meta->checksum;
-            printf( "md5: %02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x%02x\n", *(checksum++), *(checksum++), *(checksum++), *(checksum++), *(checksum++), *(checksum++), *(checksum++), *(checksum++), *(checksum++), *(checksum++), *(checksum++), *(checksum++), *(checksum++), *(checksum++), *(checksum++), *(checksum++) );
-            if ( meta->mtime != NULL ) 
-            {
-                char mtime[512];
-                strftime( mtime, 512, "%Y-%m-%d %H:%M:%S", meta->mtime );        
-                printf( "mtime: %s\n", mtime );
-            }
-            else
-            {
-                printf( "No mtime\n" );
-            }
-            printf( "size: %llu\n", meta->size );
-            printf( "object_count: %llu\n", meta->object_count );
-
-            mosso_object_meta_free( meta );    
-        }
-        else 
-        {
-            printf( "Object not found\n" );
-        }
-    }
-
-    mosso_cleanup( mosso );
-
-
-    // argv + optind
-    // argc - optind
-
-    return 0;
 }
